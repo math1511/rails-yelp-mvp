82ed4512e5b4e532a8c8ab94a943dc6e8a90ae14
matheus camargo <matheusgcamargo@hotmail.com>
2022-04-26 21:09:20 -0300

    implemented 10 restaurants seed with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
